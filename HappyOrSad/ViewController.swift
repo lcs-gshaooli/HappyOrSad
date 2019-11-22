13b735c81b051ccacf4c1e0260ce0a3913511ec2
Gabriela Shaooli <gshaooli@lcs.on.ca>
2019-11-22 14:50:00 -0500

    Add switch, case and if.

--- HappyOrSad/ViewController.swift
+++ HappyOrSad/ViewController.swift
@@ -9,25 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var enterMessage: UITextField!
-
-    @IBOutlet weak var label: UILabel!
     
+    //Mark: Properties
+    // Outlet
+    @IBOutlet weak var enterMessage: UITextField!
+    @IBOutlet weak var labelResult: UILabel!
+    
+    // MARK: Methods
+    // Functions (make things happen)
+    
+    // Runs as the view becomes visible to the user
     override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
     
     @IBAction func analyze(_ sender: Any) {
         
-        guard let enterMessageAsString = enterMessage.text, enterMessageAsString != ""
-        else {
-            label.text = "Please enter your text."
+        // 1. INPUT
+        labelResult.text = ""
+        
+        // Guard against no imput
+        guard let phraseInput = enterMessage.text, phraseInput.count > 0 else {
+            labelResult.text = "Please enter your text."
             return
-            
+        }
+        
+        // Make sure the input in not too long
+        if phraseInput.count > 255  {
+            labelResult.text = "Please enter a phrase that is no more than 255 characters"
+            return
+        }
+        // 2. PROCCES
+        print (phraseInput)
+        
+        // Create varibales
+        var Happy = 0
+        var sad = 0
     }
     
-
+    for singleCharcter in phraseInput { 
+    
+    switch single character {
+    case "😃" ,"😊","🙂", "😄":
+    emotion += 1
+    case  "☹", "🙁", "😕", "😔":
+    default
+    outputResault.text = "None"
+    
     }
+    if singleCharcter > 0 {
+    outputResult.text = "happy"
+    } if singleCharcter > 0
+    outputResult.text = "sad"
+    if singleCharcter == 0 {
+    outputResult = "none"
+    }
+}
 }
